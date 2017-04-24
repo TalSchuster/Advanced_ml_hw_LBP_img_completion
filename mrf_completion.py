436357b42de2de634aade98eb8903319cf6ee281
TalSchuster <talschuster@gmail.com>
2017-04-24 18:46:05 +0300

    Image completion

--- mrf_completion.py
+++ mrf_completion.py
@@ -8,15 +8,31 @@ import sys
 from scipy import misc
 import numpy as np
 
+VALUES = range(0, 256)
+VMAX = 50
+EPOCHS = 50
+SAVE_ITERS = False
+RECTANGLE_BORD = (92,106,13,93)
+
+def log_phi(x_i, x_j):
+    """
+    Calculates the log of pairwise function (phi)
+    :param x_i: value of pixel (can be a numpy array with all values)
+    :param x_j: value of pixel (can be a numpy array with all values)
+    :return: log of phi
+    """
+
+    return - np.minimum(np.abs(x_i - x_j), VMAX)
 
 class Vertex(object):
     def __init__(self,name='',y=None,neighs=None,in_msgs = None,observed=True):
         self._name = name
         self._y = y # original pixel
         if(neighs == None): neighs = set()  # set of neighbour nodes
-        if(in_msgs==None): in_msgs = {}  # dictionary mapping neighbours to their messages
+        if(in_msgs==None): in_msgs = {}  # dictionary mapping neighbours to the log of their messages
         self._neighs = neighs
         self._in_msgs = in_msgs
+        self.is_observed = observed
 
     def add_neigh(self,vertex):
         self._neighs.add(vertex)
@@ -24,13 +40,71 @@ class Vertex(object):
     def rem_neigh(self,vertex):
         self._neighs.remove(vertex)
 
+    def initialize_in_msgs(self):
+        for k in self._neighs:
+            #self._in_msgs[k] = np.random.random(len(VALUES))
+            self._in_msgs[k] = np.ones(len(VALUES))
+        return
+
+    def update_in_msgs(self, neigh, msgs):
+        self._in_msgs[neigh] = msgs
+        return
+
+    def get_msg(self, neigh, x_j):
+        """
+        Returns the log value of the message to the neighbor with x_j value: m_v,neigh(val).
+        Note that the message is not normalized here.
+        :param neigh: neighbor that the message is for
+        :param x_j: value of the neighbor for the message  (can be a numpy array with all values)
+        :return: log value of the message (NOT normalized)
+        """
+        if self.is_observed:
+            x_is = self._y
+        else:
+            x_is = np.array(VALUES)
+
+        sum_log_msgs = np.zeros(len(VALUES))
+        for k in self._in_msgs.keys():
+            if k == neigh:
+                continue
+
+            sum_log_msgs += self._in_msgs[k][x_is]
+
+        log_msgs = sum_log_msgs + log_phi(x_is, x_j)
+        return np.max(log_msgs)
+
     def get_belief(self):
+        """
+        Finds the value for the vertex that maximizes the messages from its neighbors.
+        :return: 
+        argmax(x_i) - the value that maximizes
+        """
+        if self.is_observed:
+            return self._y
+
+        sum_log_msgs = np.zeros(len(VALUES))
+        for k in self._neighs:
+            sum_log_msgs += self._in_msgs[k]
+
+        return np.argmax(sum_log_msgs)
+
+    def prop_all_neighbors(self):
+        for k in self._neighs:
+            self.snd_msg(k)
         return
 
     def snd_msg(self,neigh):
         """ Combines messages from all other neighbours
             to propagate a message to the neighbouring Vertex 'neigh'.
         """
+        log_msgs = np.zeros(len(VALUES))
+        for x_j in VALUES:
+            log_msgs[x_j] = self.get_msg(neigh, x_j)
+
+        # Normalizing the messages (using the log of their value)
+        log_msgs_norm = log_msgs - np.log(np.sum(np.exp(log_msgs)))
+
+        neigh.update_in_msgs(self, log_msgs_norm)
         return
 
     def __str__(self):
@@ -38,7 +112,7 @@ class Vertex(object):
         ret += "\nNeighbours:"
         neigh_list = ""
         for n in self._neighs:
-            neigh_list += " "+n._name
+            neigh_list += " "+ n._name
         ret += neigh_list
         return ret
 
@@ -110,13 +184,19 @@ def is_observed(row,col): # helper function for deciding which pixels are observ
     """
     Returns True/False by whether pixel at (row,col) was observed or not
     """
-    x1,x2,y1,y2 = 92,106,13,93 # unobserved rectangle borders for 'pinguin-img.png' 
+    x1,x2,y1,y2 = RECTANGLE_BORD # unobserved rectangle borders for 'pinguin-img.png'
     def in_rect(row,col,x1,x2,y1,y2): 
         if(row<x1 or row>x2): return False
         if(col<y1 or col>y2): return False
         return True
     return not(in_rect(row,col,x1,x2,y1,y2))
 
+def in_segment(row,col): # helper function for deciding which pixels are in the interest segment
+    """
+    Returns True/False by whether pixel at (row,col) is at the image segment of interest
+    """
+    return not is_observed(row, col) or not is_observed(row - 1, col) or not is_observed(row, col - 1) \
+                or not is_observed(row+1, col) or not is_observed(row, col+1)
 
 def build_grid_graph(n, m, img_mat):
     """ Builds an nxm grid graph, with vertex values corresponding to pixel intensities.
@@ -127,10 +207,12 @@ def build_grid_graph(n, m, img_mat):
     returns the Graph object corresponding to the grid
     """
     V = []
+    seg = []
     g = Graph()
     # add vertices:
     for i in range(n*m):
         row, col = (i//m,i%m)
+
         v = Vertex(name="v"+str(i), y=img_mat[row][col], observed = is_observed(row,col))
         g.add_vertex(v)
         if ((i%m)!=0):  # has left edge
@@ -140,8 +222,13 @@ def build_grid_graph(n, m, img_mat):
             g.add_edge((v,V[i-m]))
 
         V += [v]
+        if in_segment(row, col):
+            seg += [v]
 
-    return g, V
+    for v in V:
+        v.initialize_in_msgs()
+
+    return g, V, seg
 
 
 def grid2mat(grid,n,m):
@@ -156,25 +243,42 @@ def grid2mat(grid,n,m):
     for v in l:
         i = int(v._name[1:])
         row,col = (i//m,i%m)
-        mat[row][col] = 2017 # you should change this of course
+        mat[row][col] = v.get_belief()
     return mat
 
 # begin:
-#if len(sys.argv)<2:
-#    print 'Please specify output filename'
-#    exit(0)
+if len(sys.argv)<2:
+    print 'Please specify output filename'
+    exit(0)
+
+outfile_name = sys.argv[1]
 # load image:
 img_path = 'penguin-img.png'
 image = misc.imread(img_path)
-n, m = (107-91, 94-12)  # The missing segment plus its neighbors.
-image_segment = image[91:107, 12:94]  # here a segment of the original image should be taken
+n, m = image.shape  # The missing segment plus its neighbors.
+image_segment = image[91:108, 12:95]  # here a segment of the original image should be taken
 # build grid:
-g, V = build_grid_graph(n, m, image_segment)
+g, V, seg = build_grid_graph(n, m, image)
 # process grid:
 
-# convert grid to image: 
+for e in xrange(EPOCHS):
+    print 'Epoch %i out of %i' % (e+1, EPOCHS)
+    i = 0
+    for v in seg:
+        v.prop_all_neighbors()
+        if i % 200 == 0:
+            print '%i out of %i' % (i, len(seg))
+
+        i += 1
+
+    seg.reverse()  # Reverse the order of the vertices each on epoch.
+    if SAVE_ITERS:
+        infered_img = grid2mat(g, n, m)
+        misc.toimage(infered_img).save(outfile_name + str(e+1) + '.png')
+
+# convert grid to image:
 infered_img = grid2mat(g, n, m)
-image_final = 2017 # plug the inferred values back to the original image
+image_final = infered_img # plug the inferred values back to the original image
 # save result to output file
 outfile_name = sys.argv[1]
 misc.toimage(image_final).save(outfile_name+'.png')
